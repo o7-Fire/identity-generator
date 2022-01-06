409f1e30a7a4a1240cac655991003d9f49d48b2b
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-06 23:33:56 +0800

    Create name.py

---
+++ rlgenerate/name.py
@@ -0,0 +1,8 @@
+import random
+
+def get_name():
+	with open("../tools/firstname.txt") as f:
+		firstname = random.choice(f.read().split("\n"))
+	with open("../tools/lastname.txt") as f:
+		lastname = random.choice(f.read().split("\n"))
+	return f"{firstname} {lastname}"
