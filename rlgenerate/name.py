73149255de8112cf6f07f02287d715266772c5cb
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-07 20:33:44 +0800

    hail sys.path.append

--- rlgenerate/name.py
+++ rlgenerate/name.py
@@ -1,8 +1,11 @@
+import sys
+sys.path.append("../")
+
 import random
 
 def get_name():
-	with open("../tools/firstname.txt") as f:
+	with open("./tools/firstname.txt") as f:
 		firstname = random.choice(f.read().split("\n"))
-	with open("../tools/lastname.txt") as f:
+	with open("./tools/lastname.txt") as f:
 		lastname = random.choice(f.read().split("\n"))
 	return f"{firstname} {lastname}"
