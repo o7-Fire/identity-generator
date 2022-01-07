5a78692f124455b0d2236e5edfd17bd85b598be0
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-07 23:41:49 +0800

    Update name.py

--- rlgenerate/name.py
+++ rlgenerate/name.py
@@ -3,9 +3,32 @@ sys.path.append("../")
 
 import random
 
-def get_name():
-	with open("./tools/firstname.txt") as f:
-		firstname = random.choice(f.read().split("\n"))
+def get_first_name(gender):
+	if gender: #if True, male
+		with open("./tools/male/firstname.txt") as f:
+			firstname = random.choice(f.read().split("\n"))
+		return firstname
+	else:
+		with open("./tools/female/firstname.txt") as f:
+			firstname = random.choice(f.read().split("\n"))
+		return firstname
+		
+
+def get_last_name():
 	with open("./tools/lastname.txt") as f:
 		lastname = random.choice(f.read().split("\n"))
-	return f"{firstname} {lastname}"
+	return lastname
+
+def get_name(gender):
+	if gender:
+		with open("./tools/male/firstname.txt") as f:
+			firstname = random.choice(f.read().split("\n"))
+		with open("./tools/lastname.txt") as f:
+			lastname = random.choice(f.read().split("\n"))
+		return f"{firstname} {lastname}"
+	else:
+		with open("./tools/female/firstname.txt") as f:
+			firstname = random.choice(f.read().split("\n"))
+		with open("./tools/lastname.txt") as f:
+			lastname = random.choice(f.read().split("\n"))
+		return f"{firstname} {lastname}"
