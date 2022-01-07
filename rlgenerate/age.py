a063925598fa1515b4918b2020ee94cb8074a363
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-07 20:59:02 +0800

    who is jason

--- rlgenerate/age.py
+++ rlgenerate/age.py
@@ -1,8 +1,16 @@
 from datetime import datetime, timedelta
 from random import randint
+import math
 
 def date_of_birth_generator(minimum_age=13, maximum_age=99):
-    #format: YY-MM-DD HH-MM-SS
+    #format: {"age": int, "birthdate": "YY-MM-DD HH-MM-SS"}
     mintime = timedelta(days=365 * minimum_age)
     maxtime = timedelta(days=randint(0, 365 * (maximum_age - minimum_age)))
-    return str(datetime.today() - maxtime - mintime).split(".")[0]
+    birthdate = datetime.today() - maxtime - mintime
+    date = str(birthdate).split(".")[0]
+    age = math.floor((datetime.today() - birthdate).days / 365)
+    
+    return {
+        "age":age,
+        "birthdate":date,
+    }
