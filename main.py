4db8dd7236682f43e25dea09f42c36cc59355843
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-07 23:41:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 #TODO do the generating part
 #for now have this piece of code
 
-#import pgenerator.functions as tf
-#print(tf.generate_personality('polish', 'male'))
-
-#from onlinegenerate.username import generate_username
-#print(generate_username(10, 15))
-
-#from rlgenerate.age import date_of_birth_generator
-#print(date_of_birth_generator(15, 22))
-
-#from rlgenerate.gender import get_gender
-#print(get_gender())
-
+from onlinegenerate.username import generate_username
+from rlgenerate.age import date_of_birth_generator
 from rlgenerate.name import get_name
 from rlgenerate.extralore.parents import get_parents
+from rlgenerate.gender import get_gender
 
-name = get_name()
-print(name)
-print(get_parents(name.split(" ")[1]))
+def generate():
+	dbirth = date_of_birth_generator(15, 22)
+	gender = get_gender()
+	if gender == "male":
+		genderbool = True
+	else:
+		genderbool = False
+	name = get_name(genderbool)
+	parents = get_parents(name.split(' ')[1], genderbool)
+	print(f"online username: {generate_username(10, 15)}")
+	print(f"age: {dbirth['age']}")
+	print(f"birth date: {dbirth['birthdate']}")
+	print(f"gender: {gender}")
+	print("")
+	print(f"name: {name}")
+	print(f"father: {parents['father']}, mother: {parents['mother']}")
+	
+generate()
