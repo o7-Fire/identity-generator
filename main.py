249e9e631d2d962a1a3d864fa07b8ba746a632ab
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-07 20:49:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 #TODO do the generating part
 #for now have this piece of code
 
-from onlinegenerate.username import generate_username
-print(generate_username(10, 15))
+#import pgenerator.functions as tf
+#print(tf.generate_personality('polish', 'male'))
+
+#from onlinegenerate.username import generate_username
+#print(generate_username(10, 15))
+
+#from rlgenerate.age import date_of_birth_generator
+#print(date_of_birth_generator(15, 22))
+
+#from rlgenerate.gender import get_gender
+#print(get_gender())
+
+from rlgenerate.name import get_name
+from rlgenerate.extralore.parents import get_parents
+
+name = get_name()
+print(name)
+print(get_parents(name.split(" ")[1]))
